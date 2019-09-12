5a9983b7e690652864a45449de908b9528bfefb1
“Elyse <elysemyers1@gmail.com>
2019-09-12 15:36:06 -0600

    Adds fetch from frontend apps APIs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import $ from 'jquery';
 import './css/base.scss';
 import './images/desk-bell.svg';
 import './images/background-img.jpg';
+import Hotel from './Hotel';
 
-$('.tabs-content div').hide();
-$('.tabs-content div:first').show();
-$('.tabs-nav li:first').addClass('tab-active');
+let hotel;
 
-// Change tab class and display content
-$('.tabs-nav a').on('click', function (event) {
-  event.preventDefault();
-  $('.tabs-nav li').removeClass('tab-active');
-  $(this).parent().addClass('tab-active');
+Promise.all([
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  .then(data => data.json()), 
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(data => data.json()), 
+  fetch ('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings').then(data => data.json()), 
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
+  .then(data => data.json())])
+  .then(data => hotel = new Hotel(data[0].users, data[1].rooms, data[2].bookings, data[3].roomServices)).then(() => console.log(hotel))
+  // .then(data => hotel.instantiateHotel(data))
+
+  
+console.log(hotel)
+// function instantiateHotel(data) {
+//   hotel = new Hotel(data);
+//     console.log(hotel)
+
+//   }
+
   $('.tabs-content div').hide();
-  $($(this).attr('href')).show();
-});
+  $('.tabs-content div:first').show();
+  $('.tabs-nav li:first').addClass('tab-active');
+  
+  // Change tab class and display content
+  $('.tabs-nav a').on('click', function (event) {
+    event.preventDefault();
+    $('.tabs-nav li').removeClass('tab-active');
+    $(this).parent().addClass('tab-active');
+    $('.tabs-content div').hide();
+    $($(this).attr('href')).show();
+  });  
