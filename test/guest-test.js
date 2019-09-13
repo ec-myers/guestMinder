2c9237aa981d00b6b1d8c8342dc531a3a21c70d9
“Elyse <elysemyers1@gmail.com>
2019-09-12 18:05:31 -0600

    Adds test for guest default properties

--- test/guest-test.js
+++ test/guest-test.js
@@ -1,9 +1,9 @@
 import chai from 'chai';
 const expect = chai.expect;
-import spies from 'chai-spies';
+// import spies from 'chai-spies';
 import Guest from '../src/Guest.js'
 
-chai.use(spies);
+// chai.use(spies);
 // chai.spy.on(domUpdates,
 //   ['',
 //     '',
@@ -13,7 +13,7 @@ describe('Guest', () => {
   let guest;
 
   beforeEach( () => {
-    guest = new Guest();
+    guest = new Guest(99, 'Elyse');
   });
 
   it('should be a function', () => {
@@ -23,4 +23,12 @@ describe('Guest', () => {
   it('should be an instance of Guest', () => {
     expect(guest).to.be.an.instanceOf(Guest);
   });
+
+  it('should have an id', () => {
+    expect(guest.id).to.equal(99);
+  });
+
+  it('should have a name', () => {
+    expect(guest.name).to.equal('Elyse');
+  })
 });
